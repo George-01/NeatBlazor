88cd26c76de0430e0a381e1913a5d67867c67900
George-01 <george.coker@hotmail.com>
2025-04-04 22:37:36 +0100

    setup blazor project and implemented form controls and components

---
+++ NeatBlazor/Models/Product_Prop.cs
@@ -0,0 +1,9 @@
+﻿namespace NeatBlazor.Models
+{
+    public class Product_Prop
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
