9a75d4736f28dd0c641a737323c71921da6adad8
George-01 <george.coker@hotmail.com>
2025-04-07 07:35:07 +0100

    implemented event call back for partial and main view pages

--- NeatBlazor/Models/Product.cs
+++ NeatBlazor/Models/Product.cs
@@ -3,7 +3,7 @@
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string ProductName { get; set; }
         public double Price { get; set; }
         public bool IsActive { get; set; }
 
