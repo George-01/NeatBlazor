3ee00d516cffd6ce3f84b93d1e11b94b48255b7d
George-01 <george.coker@hotmail.com>
2025-04-17 15:49:08 +0100

    updated blazor play ground project

--- NeatBlazor/Models/Product.cs
+++ NeatBlazor/Models/Product.cs
@@ -1,12 +1,27 @@
-﻿namespace NeatBlazor.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NeatBlazor.Models
 {
     public class Product
     {
         public int Id { get; set; }
+        [Required]
         public string ProductName { get; set; }
+        [Range(1, 1000)]
         public double Price { get; set; }
         public bool IsActive { get; set; }
 
         public IEnumerable<Product_Prop> ProductProperties { get; set; }
+
+        public Category Category { get; set; }
+
+        public DateOnly AvailableAfter { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+    }
+
+    public enum Category
+    {
+        Entree,
+        Appetizer,
+        Dessert
     }
 }
